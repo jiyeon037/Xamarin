c00675f14c1fbc40528d98cf6f39b58d2feb8f15
jiyeon037 <jiyeon7269@naver.com>
2018-10-30 13:33:54 +0900

    simple phoneword translator

---
+++ PhoneWord/PhoneWord/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace PhoneWord
+{
+    [Activity(Label = "Phone Word", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            EditText phoneNumText = FindViewById<EditText>(Resource.Id.phoneNumText);
+            TextView translatedText = FindViewById<TextView>(Resource.Id.translatedText);
+            Button translateBtn = FindViewById<Button>(Resource.Id.translateBtn);
+
+            translateBtn.Click += (sender, e) =>
+            {
+                string translatedNum = Core.PhonewordTranslator.ToNumber(phoneNumText.Text);
+                if (string.IsNullOrWhiteSpace(translatedNum))
+                {
+                    translatedText.Text = string.Empty;
+                }
+                else
+                {
+                    translatedText.Text = translatedNum;
+                }
+            };
+
+        }
+    }
+}
